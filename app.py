caccbc4702dc29898f47244ee13bd9fcca2a7db7
Unknown <alexezhov169@gmail.com>
2018-06-28 16:40:03 +0300

    Articles + DB

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, flash, redirect, url_for, session, logging
+from data import Articles
+from flask_sqlalchemy import *
+from  wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -12,8 +19,27 @@ def about():
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html')
+    return render_template('articles.html', articles = Articles)
 
+@app.route('/articles/<string:id>')
+def article(id):
+    return render_template('article.html', id=id)
+
+
+
+class RegisterForm(Form):
+    name = StringField('Name',[validators.length(min=3,max=50)])
+    username = StringField('UserName',[validators.Length(min=4,max=25)])
+    email = StringField('Email',[validators.Length(min=6,max=50)])
+    password = PasswordField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message="Пароли не совпадают")
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- data.py
+++ data.py
@@ -4,36 +4,36 @@ def Articles():
         'id':'1',
         'title':'Some',
         'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'author':'',
-        'create_date':''
+        'author':'John Smith',
+        'create_date':'10-01-1950'
     },
     {
         'id':'2',
         'title':'Another',
         'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'author':'',
-        'create_date':''
+        'author':'Barbara Streisen',
+        'create_date':'02-03-2010'  
     },
     {
         'id':'3',
         'title':'Somebody',
         'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'author':'',
-        'create_date':''
+        'author':'Mendi Milcovich',
+        'create_date':'02-03-2010'
     },
     {
         'id':'4',
         'title':'Some',
         'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'author':'',
-        'create_date':''
+        'author':'Samuel Jackson',
+        'create_date':'11-11-1978'
     },
     {
         'id':'5',
         'title':'Life is Life',
         'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'author':'',
-        'create_date':''
+        'author':'Qwentin Tarantino',
+        'create_date':'16-09-1986'
     }
 
     ]
