d6e05d38e49aa1a770b2643d1a9dd122342aa4c2
Unknown <alexezhov169@gmail.com>
2018-07-04 18:29:00 +0300

    Registration+Flash Message+Mysql

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask,render_template, flash, redirect, url_for, session, logging
+from flask import Flask,render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
-from flask_sqlalchemy import *
-from  wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 
 app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'hedgehog'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MySQL
+mysql = MySQL(app)
 
 Articles = Articles()
 
@@ -41,9 +50,27 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
-    return render_template('register.html', form=form)
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
 
+        #create cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name,email,username,password))
+
+        #commit to DB
+        mysql.connection.commit()
+
+        #close connection
+        cur.close()
+
+        flash('You are now registered and con to login','success')
+
+        redirect(url_for(index))
+
+        return render_template('register.html',form=form)
+    return render_template('register.html', form=form)
 
 
 
